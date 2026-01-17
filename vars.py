7f260ba99eb362c33e1b61e83b1976779031d1a7
ritakarmakar999999-coder <ritakarmakar999999@gmail.com>
2026-01-17 09:08:44 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,12 +13,13 @@ BOT_USERNAME = getenv("BOT_USERNAME", "MyMyMyMyisnothingbhaibot")
 MONGO_URL = getenv("MONGO_URL", "")
 
 # 👤 Admin & Sudo Users
-OWNER_ID = int(getenv("OWNER_ID", "")) 
+OWNER_ID = int(getenv("OWNER_ID", "123456789")) 
+ADMINS = [OWNER_ID] # এই লাইনটি 'ADMINS' এরর ফিক্স করবে
 
-# 🖼️ Logos & Pics (আপনার ছবি না থাকলেও এই লিঙ্কগুলো দরকার)
+# 🖼️ Logos & Pics
 START_PIC = getenv("START_PIC", "https://telegra.ph/file/default.jpg")
 photologo = getenv("photologo", "https://telegra.ph/file/default.jpg")
 
-# 💳 Credit & Extra (লগের 'CREDIT' এরর ফিক্স করার জন্য)
-CREDIT = getenv("CREDIT", "Nath") # এটি যুক্ত করা হয়েছে এরর ফিক্স করতে
+# 💳 Credit & Extra
+CREDIT = getenv("CREDIT", "Nath")
 LOG_GROUP = int(getenv("LOG_GROUP", "0"))
