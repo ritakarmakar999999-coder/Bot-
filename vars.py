937dbea499e8e93d384e76a2e7f5e74f88ec5e1a
ritakarmakar999999-coder <ritakarmakar999999@gmail.com>
2026-01-17 09:34:23 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,19 +2,19 @@ import os
 from os import getenv
 
 # 🔐 API & Bot Credentials
-API_ID = int(getenv("API_ID", "0")) 
+API_ID = int(getenv("API_ID", "0"))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 # 🤖 Bot Username
-BOT_USERNAME = getenv("BOT_USERNAME", "MyMyMyMyisnothingbhaibot")
+BOT_USERNAME = getenv("BOT_USERNAME", "MyMyMyisnothingbhaibot")
 
 # 🗄️ Database URL
 MONGO_URL = getenv("MONGO_URL", "")
 
 # 👤 Admin & Sudo Users
-OWNER_ID = int(getenv("OWNER_ID", "123456789")) 
-ADMINS = [OWNER_ID] # এই লাইনটি 'ADMINS' এরর ফিক্স করবে
+OWNER_ID = int(getenv("OWNER_ID", "123456789")) # এখানে আপনার নিজের আইডি বসান
+ADMINS = [OWNER_ID] # এই লাইনটি লগের 'ADMINS' এররটি ফিক্স করবে
 
 # 🖼️ Logos & Pics
 START_PIC = getenv("START_PIC", "https://telegra.ph/file/default.jpg")
@@ -22,6 +22,5 @@ photologo = getenv("photologo", "https://graph.org/file/f70445d06b6b72d80c653.jp
 # উপরের লিঙ্কটি কার্যকর, এটি দিলে আর ছবির এরর আসবে না
 
 # 💳 Credit & Extra
-CREDIT = getenv("CREDIT", "Nath")
+CREDIT = getenv("CREDIT", "Nath") 
 LOG_GROUP = int(getenv("LOG_GROUP", "0"))
-
